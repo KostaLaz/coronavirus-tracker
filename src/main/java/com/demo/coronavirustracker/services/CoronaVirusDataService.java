9d055aae4f8380cd98ef5f309cdd1e608e561748
kosta.lazarevski@inellipse.com <kosta.lazarevski@inellipse.com>
2020-03-24 13:17:53 +0100

    Creating an HttpRequest

--- src/main/java/com/demo/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/demo/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,18 +1,24 @@
 package com.demo.coronavirustracker.services;
 
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
-    public void fetchVirusData(){
+    public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                    .uri(URI.create(VIRUS_DATA_URL))
                    .build();
-
+        HttpResponse httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
     }
 }
