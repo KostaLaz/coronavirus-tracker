25f2a63ef86805688cc6d4a347db050a30d492dc
kosta.lazarevski@inellipse.com <kosta.lazarevski@inellipse.com>
2020-03-24 13:41:41 +0100

    Parsing the CSV

--- src/main/java/com/demo/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/demo/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,13 @@
 package com.demo.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,5 +26,13 @@ public class CoronaVirusDataService {
                    .build();
         HttpResponse httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
+
+        Reader in = new FileReader("path/to/file.csv");
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+        for (CSVRecord record : records) {
+            String id = record.get("ID");
+            String customerNo = record.get("CustomerNo");
+            String name = record.get("Name");
+        }
     }
 }
